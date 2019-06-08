b8aec8129bb7a068a92e85a3002b32286cb4ece4
dromich <hrushch.rr57@gmail.com>
2019-06-08 13:46:26 +0300

    Базовий функціонал

--- content_scripts/pason.js
+++ content_scripts/pason.js
@@ -1,1 +1,33 @@
-console.log('PAsss ONNNNN')
+let inp= document.querySelectorAll('input');
+
+function PasOff() {
+	alert("Паролі буде показано");
+	for (let index = 0; index < inp.length; index++) {
+	
+		if (inp[index].type == "password") {
+			
+			inp[index].type = "text";
+			inp[index].className += " pasof";
+			
+		}
+		
+		
+	}	
+}
+
+function PasOn() {
+	alert("Паролі буде приховано");
+	for (let index = 0; index < inp.length; index++) {
+	
+		if (inp[index].classList.contains("pasof")) {
+			
+			inp[index].type = "password";
+			
+			
+		}
+		
+		
+	}	
+}
+	
+PasOff();
