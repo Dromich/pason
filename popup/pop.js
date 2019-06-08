0a6aaab4dc463d486a77d5cfa7e29ea944ba4d7e
dromich <hrushch.rr57@gmail.com>
2019-06-08 13:02:23 +0300

    Базова структура

---
+++ popup/pop.js
@@ -0,0 +1,23 @@
+
+  
+  document.addEventListener("click", (e) => {
+
+
+	browser.tabs.executeScript(null, { 
+		file: "/content_scripts/pason.js" 
+	  });
+  
+	  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+	  gettingActiveTab.then((tabs) => {
+		browser.tabs.sendMessage(tabs[0].id, {comand: 'on'});
+	  });
+
+	if (e.target.classList.contains("beast")) {
+	 
+  
+	}
+	else if (e.target.classList.contains("clear")) {
+	  browser.tabs.reload();
+	  window.close();
+	}
+  });
